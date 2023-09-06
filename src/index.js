91295efe4ea5f95af8cf7a4e7a214404d2acd026
Lawrence Caulker <locaulker@gmail.com>
2023-09-06 01:55:36 -0400

    how to create basic book list in react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-function Greeting() {
-  return <h2>My First Component</h2>
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/815oQ6G6HDL._AC_UL600_SR600,400_.jpg"
+    alt="Holly Hardcover"
+  />
+)
+const Title = () => <h2>Holly Hardcover</h2>
+const Author = () => {
+  return <h4>by Stephen King</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Greeting />)
+root.render(<BookList />)

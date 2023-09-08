893de66acb20babe9ed7d297d942472aeb6c6d7f
Lawrence Caulker <locaulker@gmail.com>
2023-09-08 00:28:11 -0400

    end simple list

--- src/index.js
+++ src/index.js
@@ -3,33 +3,22 @@ import ReactDOM from "react-dom/client"
 
 import "./index.css"
 
-const firstBook = {
-  author: "Jordan Moore",
-  title: "Interesting Facts For Curious Minds",
-  img: "./images/book-1.jpg"
-}
+const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "./images/book-1.jpg"
+  },
 
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg"
-}
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg"
+  }
+]
 
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-    </section>
-  )
+  return <section className="booklist">{books}</section>
 }
 
 const Book = props => {

d9486205014a634e3ac4f626cb659e8b57192046
Lawrence Caulker <locaulker@gmail.com>
2023-09-09 08:31:35 -0400

    end L47

--- src/index.js
+++ src/index.js
@@ -22,44 +22,11 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
-      <EventExamples />
       {books.map(book => {
         // const { img, title, author, id } = book
 
         return <Book key={book.id} book={book} />
       })}
-    </section>
-  )
-}
-
-const EventExamples = () => {
-  const handleFormInput = e => {
-    console.log(e)
-    console.log(e.target.name)
-    console.log(e.target.value)
-  }
-
-  const handleFormSubmit = e => {
-    e.preventDefault()
-    console.log("form submitted")
-  }
-
-  const handleButtonClick = () => {
-    alert("handle Button Click")
-  }
-
-  return (
-    <section>
-      <form onSubmit={handleFormSubmit}>
-        <h2>Typical form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: "1rem" }}
-        />
-        <button onClick={handleButtonClick}>Click Me</button>
-      </form>
     </section>
   )
 }
